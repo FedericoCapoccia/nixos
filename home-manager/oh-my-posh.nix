eb9ba05480e2e490fc219174543399d534c2fa9a
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2024-07-12 20:12:14 +0200

    Here be Dragons

--- home-manager/oh-my-posh.nix
+++ home-manager/oh-my-posh.nix
@@ -5,7 +5,7 @@ in
 {
   home = {
     packages = [ pkgs.oh-my-posh ];
-    file."${ohmyposhConfig}/zen.toml".text = builtins.readFile ../.config/oh-my-posh/zen.toml;
+    file."${ohmyposhConfig}/zen.toml".text = builtins.readFile ./config/oh-my-posh/zen.toml;
   };
   programs.zsh.initExtra = ''
     eval "$(${pkgs.oh-my-posh}/bin/oh-my-posh init zsh --config ${ohmyposhConfig}/zen.toml)"
