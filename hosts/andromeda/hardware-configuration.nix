5950a9d003ead9547c21664a6ba6e95cf8aa2261
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2025-04-02 23:13:34 +0200

    .

--- hosts/andromeda/hardware-configuration.nix
+++ hosts/andromeda/hardware-configuration.nix
@@ -1,9 +1,22 @@
-{ config, lib, modulesPath, ... }: {
+{
+  config,
+  lib,
+  modulesPath,
+  systemConfig,
+  ...
+}:
+{
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
   boot.initrd = {
-    availableKernelModules =
-      [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+    availableKernelModules = [
+      "nvme"
+      "xhci_pci"
+      "ahci"
+      "usbhid"
+      "usb_storage"
+      "sd_mod"
+    ];
     supportedFilesystems = [ "btrfs" ];
     kernelModules = [ ];
     verbose = false;
@@ -12,7 +25,12 @@
 
   boot = {
     kernelModules = [ "kvm-amd" ];
-    kernelParams = [ "quiet" "splash" "udev.log_level=3" "amd_pstate=passive" ];
+    kernelParams = [
+      "quiet"
+      "splash"
+      "udev.log_level=3"
+      "amd_pstate=passive"
+    ];
     extraModulePackages = [ ];
     consoleLogLevel = 0;
   };
@@ -29,8 +47,6 @@
     };
   };
 
-
-
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos-root";
     fsType = "ext4";
@@ -40,14 +56,19 @@
     device = "/dev/disk/by-label/NIXOS_BOOT";
     fsType = "vfat";
     options = [
-      "fmask=0077" "dmask=0077" 
+      "fmask=0077"
+      "dmask=0077"
     ];
+  };
+
+  fileSystems."/home/${systemConfig.username}/NVME" = {
+    device = "/dev/disk/by-uuid/68f9cc72-a314-4548-a8b8-44b6bc38c92d";
+    fsType = "ext4";
   };
 
   swapDevices = [ ];
 
   networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode =
-    lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
