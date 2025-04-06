599fdc162b3968de00b0f8e9124c41c29ba67dda
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2025-04-07 01:46:27 +0200

    systemd service for linking the wayland socket because it is not located in /run/user/1000/ for some reason but it is in /mnt/wslg/runtime-dir

--- hosts/zephyr/configuration.nix
+++ hosts/zephyr/configuration.nix
@@ -5,4 +5,17 @@
     ../shared_packages.nix
   ];
   wsl.defaultUser = "fede";
+
+  systemd.services.symlink_wayland_socket = {
+    enable = true;
+    description = "Symlink Wayland socket to XDG_RUNTIME_DIR";
+    wantedBy = [ "default.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      User = systemConfig.username;
+      ExecStart = ''
+        ${pkgs.coreutils}/bin/ln -sf /mnt/wslg/runtime-dir/wayland-0 /run/user/1000/.
+      '';
+    };
+  };
 }
